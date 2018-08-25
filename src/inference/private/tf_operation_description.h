ae73a4918619cfb0331de47953294661cb15a7e0
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2018-08-25 22:19:45 +0300

    added operation description

---
+++ src/inference/private/tf_operation_description.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "tf_base.h"
+
+namespace tensorflow_c_api
+{
+    class operation_description : private not_copyable
+    {
+
+    public:
+        operation_description(TF_Graph* b)
+        {
+            m_value = TF_NewOperation(b, "test", "name");
+        }
+
+        ~operation_description()
+        {
+            TF_DeleteOper(m_value);
+        }
+
+        operation_description(operation_description&& o)
+        {
+            m_value = o.m_value;
+            o.m_value = nullptr;
+        }
+
+        operation_description& operator=(operation_description&& o)
+        {
+            if (&o != this)
+            {
+                m_value = o.m_value;
+                o.m_value = nullptr;
+            }
+        }
+
+        operator TF_OperationDescription *() const
+        {
+            return m_value;
+        }
+
+    private:
+        TF_OperationDescription * m_value;
+    };
+}
