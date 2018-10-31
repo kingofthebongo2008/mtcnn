deb7c471784ce5edff2830358d28663d454bf91f
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2018-10-31 08:13:40 +0200

    tensorlite first graph evaluation

--- src/inference/private/main.cpp
+++ src/inference/private/main.cpp
@@ -2,12 +2,76 @@
 
 #include <tensorflow/contrib/lite/experimental/c/c_api.h>
 
+#include <memory>
+
+namespace tensorflow_lite_c_api
+{
+    struct TFL_Model_Deleter
+    {
+        void operator()(void* v)
+        {
+            TFL_DeleteModel( reinterpret_cast<TFL_Model*>(v) );
+        }
+    };
+
+    using model = std::unique_ptr< TFL_Model, TFL_Model_Deleter >;
+
+
+    struct TFL_InterpreterOptions_Deleter
+    {
+        void operator()(void* v)
+        {
+            TFL_DeleteInterpreterOptions(reinterpret_cast<TFL_InterpreterOptions*>(v));
+        }
+    };
+
+    using interpreter_options = std::unique_ptr< TFL_InterpreterOptions, TFL_InterpreterOptions_Deleter >;
+
+
+    struct TFL_Interpreter_Deleter
+    {
+        void operator()(void* v)
+        {
+            TFL_DeleteInterpreter(reinterpret_cast<TFL_Interpreter*>(v));
+        }
+    };
+
+    using interpreter = std::unique_ptr< TFL_Interpreter, TFL_Interpreter_Deleter >;
+
+}
+
 
 
 int32_t main(int32_t, char**)
 {
-    TFL_Model* model = TFL_NewModelFromFile("data/hello_world.tflite");
+    tensorflow_lite_c_api::model                m(TFL_NewModelFromFile("data/hello_world.tflite"));
+    tensorflow_lite_c_api::interpreter_options  o(TFL_NewInterpreterOptions());
+    TFL_InterpreterOptionsSetNumThreads(o.get(), 4);
+
+    tensorflow_lite_c_api::interpreter          i(TFL_NewInterpreter(m.get(), o.get()));
+
+    auto in                                     = TFL_InterpreterGetInputTensorCount(i.get());
+    auto out                                    = TFL_InterpreterGetInputTensorCount(i.get());
+
+    auto t0                                     = TFL_InterpreterGetInputTensor(i.get(), 0);
+    auto t1                                     = TFL_InterpreterGetOutputTensor(i.get(), 0);
+
+    auto s                                      = TFL_InterpreterAllocateTensors(i.get());
+
+    auto t2                                     = TFL_TensorData(t0);
+    auto t3                                     = TFL_TensorData(t1);
+
+    auto sz0 = TFL_TensorByteSize(t0);
+    auto sz1 = TFL_TensorByteSize(t1);
+
+    float x = 2.0f;
+    float y = 0.0f;
+
+    auto s1                                     = TFL_TensorCopyFromBuffer(t0, &x, sizeof(x));
+    auto s2                                     = TFL_InterpreterInvoke(i.get());
+    auto s3                                     = TFL_TensorCopyToBuffer(t1, &y, sizeof(y));
     
     
+
     return 0;
 }
