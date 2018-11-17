8b4ec903d2ca5130514cfd630cdab3f593c76800
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2018-11-17 09:32:35 +0200

    Update tfl_interpreter_helper.h

--- src/inference/private/tfl_interpreter_helper.h
+++ src/inference/private/tfl_interpreter_helper.h
@@ -5,14 +5,24 @@
 
 namespace tensorflow_lite_c_api
 {
-    inline input_tensor make_input_tensor(interpreter* i, int32_t index)
+    inline input_tensor make_input_tensor(const interpreter* i, int32_t index)
     {
         return  input_tensor(i->get_input_tensor(index));
     }
 
-    inline output_tensor make_output_tensor(interpreter* i, int32_t index)
+    inline output_tensor make_output_tensor(const interpreter* i, int32_t index)
     {
         return  output_tensor(i->get_output_tensor(index));
+    }
+
+    inline input_tensor make_input_tensor(const interpreter& i, int32_t index)
+    {
+        return  input_tensor(i.get_input_tensor(index));
+    }
+
+    inline output_tensor make_output_tensor(const interpreter& i, int32_t index)
+    {
+        return  output_tensor(i.get_output_tensor(index));
     }
 
 }
